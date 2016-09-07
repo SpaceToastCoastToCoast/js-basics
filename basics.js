185f4fc88780c07a47f995cfb95aeb25bc4f7668
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-06 17:06:37 -1000

    passed seventh test: dataTypes includes null, undefined, object, boolean, number, and string

--- basics.js
+++ basics.js
@@ -26,6 +26,7 @@ var myName = "Alan";
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
  */
+ var dataTypes = [1, true, "word", undefined, {}, null];
 
 /* Create a `dog` object
  * it should have a `bark` function that makes your dog bark!
